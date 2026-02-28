1b1bcc45d969d7c2b8f54f356faf5cc8895e1c2a
Gaspi <gaspar_peralta09@hotmail.com>
2026-02-28 01:11:26 +0100

    configure pug view engine and render index with variables

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
 'use strict';
 require('dotenv').config();
+
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
+/* ---------- PUG CONFIG ---------- */
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
 
-fccTesting(app); //For FCC testing purposes
+/* ---------- FCC TESTING ---------- */
+fccTesting(app);
+
+/* ---------- MIDDLEWARE ---------- */
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+/* ---------- ROUTES ---------- */
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {
+    title: 'Hello from Pug',
+    message: 'Rendering with Pug!',
+    showLogin: true,
+    showRegistration: true,
+    showSocialAuth: true
+  });
 });
 
+/* ---------- SERVER ---------- */
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, function () {
   console.log('Listening on port ' + PORT);
 });
+
+module.exports = app;
